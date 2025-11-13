b39cf4e7b52f56f34742a2056d31f14f6e6e6825
carinealencar <carinealencar@outlook.com>
2025-11-13 10:30:20 -0300

    sim

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title('Meu primeiro APP 🤳')
+
+st.header('Vamos fazer algo interativo')
+
+n = st.number_input('Insira o número')
+st.write(f'O número que você digitou elevado ao quadrado é {n**2}')
